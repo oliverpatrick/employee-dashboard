fadcc14b80cded335c94190bcf2530fd8983721e
oliverpatrick <opholroyd@gmail.com>
2022-03-10 15:56:00 +0000

    stress

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/employee-dashboard',
-  assetPrefix: '/employee-dashboard/'
+  assetPrefix: 'https://oliverpatrick.github.io//employee-dashboard/'
 };
 
 module.exports = nextConfig;
