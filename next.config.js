eb2a09a29c4057c716a5d5da590395e7d5b3f7ae
oliverpatrick <opholroyd@gmail.com>
2022-11-21 16:58:52 +0000

    fresh start

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-  // basePath: '/employee-dashboard',
-  // assetPrefix: 'https://oliverpatrick.github.io/employee-dashboard/'
-};
-
-module.exports = nextConfig;
